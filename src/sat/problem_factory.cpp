43a3db57f32a8daf36a10941782af9f19b8a61f7
Joe <mitchelljoe3@gmail.com>
2016-12-30 22:42:05 -0500

    Added commentary
    To problem_factory and random.

--- src/sat/problem_factory.cpp
+++ src/sat/problem_factory.cpp
@@ -12,14 +12,17 @@ namespace sat {
 
 		constexpr auto escape_num = 20;
 
+		// 3-SAT and expecting no duplicates, so need at least three literals.
 		if (num_nodes < 3) throw std::exception("Not enough nodes to construct a clause.");
 
 		auto clause_init = std::vector<clause_data>();
 
 		for(unsigned int clause_idx = 0; clause_idx < num_clauses; ++clause_idx) {
 
+			// Generate a clause by first getting three distinct nodes randomly...
 			auto nums = jj_random::rand_vec_less_than(num_nodes, 3);
 			auto nodes = clause_data::node_storage{ nums[0], nums[1], nums[2] };
+			// ...and then flip a coin for each literals sgn.
 			auto sgns = clause_data::sgn_storage{ coin_flip(), coin_flip(), coin_flip() };
 			clause_init.emplace_back(nodes, sgns);
 
--- src/util/random.h
+++ src/util/random.h
@@ -8,13 +8,16 @@ namespace jj_random {
 	
 	// Boolean
 
+	// 50/50 coin flip
 	bool coin_flip();
 
 
 	// Integer
 
+	// Random integer btwn min and max, inclusive
 	unsigned rand_inclusive(unsigned min, unsigned max);
 	
+	// Random integer in [0, num)
 	inline unsigned rand_less_than(unsigned int num) {
 		return rand_inclusive(0, int(num - 1));
 	}
@@ -22,14 +25,18 @@ namespace jj_random {
 
 	// Int arrays
 
+	// A vector of size vec_size with random numbers in [0, num),
+	//  with or without possible duplicates
 	std::vector<unsigned> rand_vec_less_than(
 		unsigned num, unsigned vec_size, bool with_duplicates = false);
 
 
 	// Double
 	
+	// Random number btwn 0 and 1
 	double unit_rand();
 
+	// Random number btwn min and max
 	inline double rand_btwn(double min, double max) {
 		return min + unit_rand()*max;
 	}
